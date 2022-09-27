1637684ad1c6e2852e8e74c9ebec3c3c139e62b5
arizoide <ari.thomazini@gmail.com>
2022-09-27 20:26:31 -0300

    Correção

--- src/main/java/com/senac/SenacBet/controller/ContatoController.java
+++ src/main/java/com/senac/SenacBet/controller/ContatoController.java
@@ -4,7 +4,9 @@ import com.senac.SenacBet.dao.ContatoDAO;
 import com.senac.SenacBet.model.Contato;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,6 +24,18 @@ public class ContatoController {
     public String processarContato(Contato contato){
         dao.save(contato);
         return "contato";
+    }
+
+    @GetMapping("/listar-contatos")
+    public String listar(Model model){
+        model.addAttribute("contatos", dao.findAll());
+        return "lista-contato";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String apagarContato(@PathVariable("id") Integer id, Model model){
+        dao.deleteById(id);
+        return "redirect:/listar-contatos";
     }
 
 }
